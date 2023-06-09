a9b42ed60cfadab8937b4735d719892ffa56f0d5
NiengeDavid <97910242+NiengeDavid@users.noreply.github.com>
2023-06-09 21:07:49 +0100

    Reactions uploaded via upload by Nienge David
    Reactions added; scrollability, hover effect on blog buttons and lot others

--- index.js
+++ index.js
@@ -8,25 +8,24 @@ btn.addEventListener('click', () => {
 })
 
 //Scroll effects for sm
+// Selected all menu buttons
+const menuButtons = document.querySelectorAll(".menu-button");
 
-// Select all menu buttons
-const menuButtons = document.querySelectorAll('.menu-button');
-
-// Attach click event listener to each button
+//click event listener to each button
 menuButtons.forEach(button => {
-  button.addEventListener('click', scrollToSection);
+  button.addEventListener("click", scrollToSection);
 });
 
 // Scroll to the target section
 function scrollToSection(event) {
   // Get the target section ID from the button's data-target attribute
-  const targetSectionId = event.target.getAttribute('data-target');
+  const targetSectionId = event.target.getAttribute("data-target");
 
   // Find the target section element
   const targetSection = document.getElementById(targetSectionId);
 
   // Scroll to the target section using smooth behavior
-  targetSection.scrollIntoView({ behavior: 'smooth' });
+  targetSection.scrollIntoView({ behavior: "smooth" });
 }
 
 
